328ecf45fbda62655919871107fa8ac701dd195d
wrzonki <wrzonki@gmail.com>
2019-11-02 01:27:36 +0100

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+    $('.tooltipped').tooltip();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
+});
